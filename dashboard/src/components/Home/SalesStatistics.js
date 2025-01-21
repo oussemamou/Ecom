bceb518fa47d6f37602fea88405b31de32de9b17
oussemamou <oussemamourali1@gmail.com>
2025-01-21 23:07:50 +0100

    Initial commit

---
+++ dashboard/src/components/Home/SalesStatistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SaleStatistics = () => {
+  return (
+    <div className="col-xl-6 col-lg-12">
+      <div className="card mb-4 shadow-sm">
+        <article className="card-body">
+          <h5 className="card-title">Sale statistics</h5>
+          <iframe
+            title="Sales Statistics Chart"
+            style={{
+              background: "#FFFFFF",
+              border: "none",
+              borderRadius: "2px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
+            }}
+            src="https://charts.mongodb.com/charts-test-ksjcs/public/dashboards/65288381-576e-49b0-8191-0bb27714a73c"
+          ></iframe>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SaleStatistics;
---
+++ server/data/Products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    name: "JACK_Miel",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "JACK_Miel",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "fhemt zokomhaaa mtaa zebiii hedhiii ",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "bjeh rabi ya produit odher",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Lace Walking Shoes For Boys & Girls  (Pink)",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 80,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Women Red Heels Sandal",
+    image: "https://www.aeliadutyfree.fr/media/catalog/product/cache/image/1200x/17f82f742ffe127f42dca9de82fb58b1/8/a/8a6b996b0e5aa0be8cb0acb65b8468c742a73776_100227925_1.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
